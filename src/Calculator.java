4b6088d20345880e325443241cf7bfc868e3587c
LauraMartinezO <lauramartinezo182@gmail.com>
2024-08-09 14:10:17 -0500

    Sum and substract methods added

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,12 @@
 package src;
 
 public class Calculator {
+
+    public double sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public double substract(int num1, int num2) {
+        return num1-num2;
+    }
 }
